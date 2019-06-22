9507d260b0e1c8833724d04f503a1de816592ad0
Italo Gustavo <italogustavo207@gmail.com>
2019-06-22 18:56:24 -0300

    Ajustes

--- Italo-Projeto/tela_3_cadastrar_livraria.php
+++ Italo-Projeto/tela_3_cadastrar_livraria.php
@@ -9,16 +9,16 @@
 
 		<form action="_inserir_dados_livraria.php" method="post">
 		<label>Livraria</label><br>
-		<input type="text" name="Nome_livraria" placeholder="Ex.SARAU">
+		<input type="text" name="Nome_livraria" size="60" maxlength="100" placeholder="Ex.SARAU" style="text-align: center;">
+		
+		<br><label>Endereco</label><br>
+		<input type="text" name="Endereco_livraria" size="60" maxlength="100" placeholder="Ex.Wall Street" style="text-align: center;">
 
 		<br><label>CNPJ</label><br>
-		<input type="number" name="CNPJ_livraria" placeholder="Ex.00.000.000/0000-00">
-
-		<br><label>Endereco</label><br>
-		<input type="text" name="Endereco_livraria" placeholder="Ex.Wall Street">
+		<input type="number" name="CNPJ_livraria" size="60" maxlength="100" placeholder="Ex.00.000.000/0000-00" style="text-align: center;">
 
 		<br><label>Telefone</label><br>
-		<input type="number" name="Telefone_livraria" placeholder="Ex.(000)0000-0000">
+		<input type="number" name="Telefone_livraria" size="60" maxlength="100" placeholder="Ex.(000)0000-0000" style="text-align: center;">
 
 		<br><br><input type="submit" value="CADASTRAR" ><br>
 		</form>
--- Italo-Projeto/tela_3_cadastrar_livro.php
+++ Italo-Projeto/tela_3_cadastrar_livro.php
@@ -9,16 +9,16 @@
 
 		<form action="_inserir_dados_livros.php" method="post">
 		<label>Nome</label><br>
-		<input type="text" name="Nome_Livro" size="60" maxlength="100" placeholder="As crônicas de Nárnia: O leão, a feiticeira e o guarda-roupa" style="text-align: center;">
+		<input type="text" name="Nome_Livro" size="60" maxlength="100" placeholder="Ex.As crônicas de Nárnia: O leão, a feiticeira e o guarda-roupa" style="text-align: center;">
 
 		<br><label>Autor</label><br>
-		<input type="text" name="Autor_Livro" size="60" maxlength="100" placeholder="" style="text-align: center;">
-
-		<br><label>ISBN</label><br>
-		<input type="number" name="ISBN_Livro" size="80" maxlength="100" placeholder="" style="text-align: center;">
+		<input type="text" name="Autor_Livro" size="60" maxlength="100" placeholder="Ex.Clive Staples Lewis" style="text-align: center;">
 
 		<br><label>Editora</label><br>
-		<input type="text" name="Editora_Livro" size="60" maxlength="100" placeholder="" style="text-align: center;">
+		<input type="text" name="Editora_Livro" size="60" maxlength="100" placeholder="Ex.ABU" style="text-align: center;">
+		
+		<br><label>ISBN</label><br>
+		<input type="number" name="ISBN_Livro" size="80" maxlength="100" placeholder="Ex.9781624053658" style="text-align: center;">
 
 		<br><br><input type="submit" value="CADASTRAR" ><br>
 		</form>
